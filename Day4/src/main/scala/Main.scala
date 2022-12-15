65f35ff2b4658a4e8a4c522a03e2db3b05eb7b38
Dominik Fedora Desktop <dominik151099@outlook.de>
2022-12-15 13:22:53 +0100

    Added function to solve first half

--- Day4/src/main/scala/Main.scala
+++ Day4/src/main/scala/Main.scala
@@ -4,13 +4,14 @@ import scala.io.Source
 object CampCleanUp extends App{
 
   val fileName = "/opt/AdventOfCode2022/Day4/src/main/scala/realInput.txt"
-
   val file = Source.fromFile(fileName)
+
+  var counter: Int = 0
   for(line <- file.getLines()) {
     val array: Array[Int] = parseInput(line)
-    array.foreach(n => print(n + " "))
-    println()
+    if(checkForContainingSectors(array)) counter += 1
   }
+  println(counter)
   file.close()
 
 
@@ -21,12 +22,21 @@ object CampCleanUp extends App{
    * @param input
    * @return
    */
-  def parseInput(input: String): Array[Int] = {
+  private def parseInput(input: String): Array[Int] = {
     var parsedValuesAsString = Array[Int]()
     val trimmedList: List[String] = input.split("-").map(_.trim).toList
     trimmedList.foreach(element => element.split(",").foreach(number => {parsedValuesAsString =   parsedValuesAsString :+ number.toInt}))
     parsedValuesAsString
   }
+
+  private def checkForContainingSectors(sectorArray: Array[Int]): Boolean = {
+    // first elf contains the second one
+    if(sectorArray(0) <= sectorArray(2) && sectorArray(1) >= sectorArray(3)) true
+    // second elf contains first one
+    else if(sectorArray(0) >= sectorArray(2) && sectorArray(1) <= sectorArray(3)) true
+    else false
+  }
+
 
 
 
